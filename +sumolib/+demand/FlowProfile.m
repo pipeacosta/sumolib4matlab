4538ea4bdc89c36e75415de8ed4215ce58944fb7
afacostag <afacostag@231b66dd-0bd5-4336-be68-b46fc28e7c7d>
2015-03-02 17:57:51 +0000

    This update includes several corrections suggested by the community. Thank you everybody!
    -Included the specification of the lane in the FlowProfile class
    -Included the "interseccion_aislada" scenario for the demand example. Thanks to Christian Portilla for building it
    -included the java source code of the NetReader implementation

--- +sumolib/+demand/FlowProfile.m
+++ +sumolib/+demand/FlowProfile.m
@@ -1,8 +1,9 @@
 classdef FlowProfile
 %FlowProfile Construct a FlowProfile representing a time-dependent vehicle demand.
-%   myFlowProfile = FlowProfile(FROMEDGE,PROFILE,TURNINGRATIO) Constructs a 
+%   myFlowProfile = FlowProfile(FROMEDGE,FROMLANE,PROFILE,TURNINGRATIO) Constructs a 
 %   FlowProfile object. FROMEDGE is the id of the edge from which vehicles
-%   are generated. PROFILE is a vector containing the samples of the
+%   are generated. FROMLANE specifies the edge's lane number, according to 
+%   the convention used by SUMO. PROFILE is a vector containing the samples of the
 %   time-dependent demand. Finally, TURNINGRATIO is a containers.Map object
 %   that maps the name of the edges connected to FROMEDGE, to the
 %   corresponding turning probability.
@@ -14,34 +15,27 @@ classdef FlowProfile
 
 	properties
 		fromEdge
+        fromLane
 		profile
 		turningRatio
 % 		beginTime = 0
 % 		endTime = 24*3600
 	end
 	methods
-		function this = FlowProfile(fromEdge, profile,...
-				turningRatio)
+		function this = FlowProfile(fromEdge, fromLane, profile, turningRatio)
 			
-			
-			
-% 			p = inputParser;
-% 			p.FunctionName = 'FlowProfile';
-% 			p.addRequired('fromEdge',@ischar);
-% 			p.addRequired('profile',@isnumeric);
-% 			p.addRequired('turningRatio',@(x) isa(x, 'containers.Map'));
-% 			p.addOptional('beginTime', 0, @(x) isnumeric(x) && length(x) == 1 && x >= 0);
-% 			p.addOptional('endTime', 24*3600, @(x) isnumeric(x) && length(x) == 1 && x >= this.beginTime);
-% 			p.parse(fromEdge, profile, turningRatio, varargin{:});
-% 			this.fromEdge = p.Results.fromEdge;
-% 			this.profile = p.Results.profile;
-% 			this.turningRatio = p.Results.turningRatio;
-% 			this.beginTime = p.Results.beginTime;
-% 			this.endTime = p.Results.endTime;
+			p = inputParser;
+			p.FunctionName = 'FlowProfile';
+			p.addRequired('fromEdge',@ischar);
+            p.addRequired('fromLane',@isnumeric);
+			p.addRequired('profile',@isnumeric);
+			p.addRequired('turningRatio',@(x) isa(x, 'containers.Map'));
+			p.parse(fromEdge, fromLane, profile, turningRatio);
+			this.fromEdge = p.Results.fromEdge;
+            this.fromLane = p.Results.fromLane;
+			this.profile = p.Results.profile;
+			this.turningRatio = p.Results.turningRatio;
 
-			this.fromEdge = fromEdge;
-			this.profile = profile;
-			this.turningRatio = turningRatio;
 		end
 	end
 end
